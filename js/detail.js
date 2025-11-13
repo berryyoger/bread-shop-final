3cd4169f313812ae2091fabbc2b0a8c4cd626e46
berryyoger <berry.yoger@gmail.com>
2025-11-13 22:46:33 +0900

    해결 완

--- js/detail.js
+++ js/detail.js
@@ -1,24 +1,19 @@
 const params = new URLSearchParams(location.search);
 const breadKey = params.get("bread");
-console.log("bread param:", breadKey);
+console.log(breadKey);
 
-function findProduct(bread) {
-    return bread.var === breadKey;
-  }
-
-console.log(breadList.find(findProduct))
 
 function buy(bread) {
     const buy = document.createElement("article")
     buy.classList.add("buyArticle")
     buy.innerHTML = `
         <section class="buyInfo">
-            <img src="/assets/`+ bread +`.png" alt="">
+            <img src="/assets/`+ bread.var +`.png" alt="">
         </section>
         <section class="buyBuy">
             <h1>` + bread.name + `</h1>
-            <h2>Soft yeast-raised donut with a thin, shiny sugar glaze and light bite</h2>
-            <h3>We fry the dough until golden and light, then coat it with a thin sugar glaze that sets to a gentle shine. Soft, airy, and not too sweet—simple and classic.</h3>
+            <h2>`+ bread.meta+`</h2>
+            <h3>`+ bread.onPage+`</h3>
             <form action="">
                 <input type="number" value="count" placeholder="amount">
                 <input type="radio">
@@ -26,9 +21,10 @@ function buy(bread) {
                 <input type="submit" value="Add to cart">
             </form>
         </section>`;
-    document.body.append("buy");
+    document.body.append(buy);
 }
 
 document.addEventListener("DOMContentLoaded", () =>{
-    buy(breadKey)
+    const product = breadList.find(item => item.var === breadKey);
+    buy(product);
 })
